588032e1144e9150e71e61d97984666cfa0f3656
Jacob Blaylock <jacobblaylock@gmail.com>
2017-07-28 23:04:10 -0600

    Add routing using React Router

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookSearch from './BookSearch.js'
 import MyBooks from './MyBooks.js'
 
 class BooksApp extends Component {
-  constructor(props) {
-    super(props)
-
-    this.toggleSearchPage = this.toggleSearchPage.bind(this)
-  }
-
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  toggleSearchPage() {
-    this.setState((state) => {
-      state.showSearchPage = !this.state.showSearchPage
-    })    
-  }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <BookSearch 
-            onBackClick={this.toggleSearchPage}  
-          />
-        ) : (
-          <MyBooks
-            onAddClick={this.toggleSearchPage}
-          />
-        )}
+        <Route path='/search' render={() =>(
+          <BookSearch/>
+        )}/>
+        <Route exact path='/' render={() => (
+          <MyBooks/>
+        )}/>
       </div>
     )
   }
--- src/MyBooks.js
+++ src/MyBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import BookCategory from './BookCategory.js'
 
 const bookList = [
@@ -70,7 +71,7 @@ class MyBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={this.props.onAddClick}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
